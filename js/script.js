cd1a97236ff9d6538a1a534a89859271362e4f2d
Ali Shabani <A.Shabani@KNR.local>
2025-02-15 11:23:06 +0330

    fix Monthly report update form

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// تعویض بین حالت ماهانه و چند ماهه
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll('input[name="reportType"]').forEach((elem) => {
     elem.addEventListener("change", function () {
@@ -12,8 +11,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
-
-// نمایش لودر
 const showLoading = () => {
   const loadingOverlay = document.createElement("div");
   loadingOverlay.className = "loading-overlay";
@@ -24,7 +21,6 @@ const showLoading = () => {
   document.body.classList.add("loading");
 };
 
-// پنهان کردن لودر
 const hideLoading = () => {
   const loadingOverlay = document.querySelector(".loading-overlay");
   if (loadingOverlay) {
@@ -32,8 +28,6 @@ const hideLoading = () => {
     document.body.classList.remove("loading");
   }
 };
-
-// نمایش ارور به صورت پاپ‌آپ
 const showErrorPopup = (message) => {
   const errorPopup = document.createElement("div");
   errorPopup.className = "error-popup";
@@ -50,7 +44,6 @@ const showErrorPopup = (message) => {
     }, 3000); // زمان نمایش پاپ‌آپ به میلی‌ثانیه
   }, 100);
 };
-//انیمیشن شمارش اعداد
 const animateRandomNumber = (element, end, duration) => {
   const startTime = Date.now();
   const interval = setInterval(() => {
@@ -73,69 +66,185 @@ const animateAllRandomNumbers = () => {
     }
   });
 };
-
-// ساخت فرم اینپوت در فرم گزارش
-const createInputForm = (year, month) => {
-  return `
-    <div class="form-group">
-      <input type="text" id="reportYear" name="year" placeholder=" " value="${year}" required />
-      <label for="reportYear">سال را وارد کنید</label>
-    </div>
-    <div class="form-group">
-      <div class="select-wrapper">
-        <select id="reportMonth" name="month" required>
-          <option value="" disabled ${!month ? "selected" : ""}></option>
-          ${[
-            "فروردین",
-            "اردیبهشت",
-            "خرداد",
-            "تیر",
-            "مرداد",
-            "شهریور",
-            "مهر",
-            "آبان",
-            "آذر",
-            "دی",
-            "بهمن",
-            "اسفند",
-          ]
-            .map(
-              (m, i) =>
-                `<option value="${(i + 1).toString().padStart(2, "0")}" ${
-                  month === (i + 1).toString().padStart(2, "0")
-                    ? "selected"
-                    : ""
-                }>${m}</option>`
-            )
-            .join("")}
-        </select>
-        <label for="reportMonth">ماه را انتخاب کنید</label>
+const createInputForm = (
+  reportType,
+  year,
+  month,
+  startYear,
+  startMonth,
+  endYear,
+  endMonth
+) => {
+  if (reportType === "monthly") {
+    return `
+      <div class="form-group">
+        <input type="text" id="reportYear" name="year" placeholder=" " value="${year}" required />
+        <label for="reportYear">سال را وارد کنید</label>
       </div>
-    </div>
-  `;
+      <div class="form-group">
+        <div class="select-wrapper">
+          <select id="reportMonth" name="month" required>
+            <option value="" disabled ${!month ? "selected" : ""}></option>
+            ${[
+              "فروردین",
+              "اردیبهشت",
+              "خرداد",
+              "تیر",
+              "مرداد",
+              "شهریور",
+              "مهر",
+              "آبان",
+              "آذر",
+              "دی",
+              "بهمن",
+              "اسفند",
+            ]
+              .map(
+                (m, i) =>
+                  `<option value="${(i + 1).toString().padStart(2, "0")}" ${
+                    month === (i + 1).toString().padStart(2, "0")
+                      ? "selected"
+                      : ""
+                  }>${m}</option>`
+              )
+              .join("")}
+          </select>
+          <label for="reportMonth">ماه را انتخاب کنید</label>
+        </div>
+      </div>
+    `;
+  } else {
+    return `
+      <div class="form-group">
+        <input type="text" id="startYear" name="startYear" placeholder=" " value="${
+          startYear || ""
+        }" required />
+        <label for="startYear">سال شروع را وارد کنید</label>
+      </div>
+      <div class="form-group">
+        <div class="select-wrapper">
+          <select id="startMonth" name="startMonth" required>
+            <option value="" disabled ${!startMonth ? "selected" : ""}></option>
+            ${[
+              "فروردین",
+              "اردیبهشت",
+              "خرداد",
+              "تیر",
+              "مرداد",
+              "شهریور",
+              "مهر",
+              "آبان",
+              "آذر",
+              "دی",
+              "بهمن",
+              "اسفند",
+            ]
+              .map(
+                (m, i) =>
+                  `<option value="${(i + 1).toString().padStart(2, "0")}" ${
+                    startMonth === (i + 1).toString().padStart(2, "0")
+                      ? "selected"
+                      : ""
+                  }>${m}</option>`
+              )
+              .join("")}
+          </select>
+          <label for="startMonth">ماه شروع را انتخاب کنید</label>
+        </div>
+      </div>
+      <div class="form-group">
+        <input type="text" id="endYear" name="endYear" placeholder=" " value="${
+          endYear || ""
+        }" required />
+        <label for="endYear">سال پایان را وارد کنید</label>
+      </div>
+      <div class="form-group">
+        <div class="select-wrapper">
+          <select id="endMonth" name="endMonth" required>
+            <option value="" disabled ${!endMonth ? "selected" : ""}></option>
+            ${[
+              "فروردین",
+              "اردیبهشت",
+              "خرداد",
+              "تیر",
+              "مرداد",
+              "شهریور",
+              "مهر",
+              "آبان",
+              "آذر",
+              "دی",
+              "بهمن",
+              "اسفند",
+            ]
+              .map(
+                (m, i) =>
+                  `<option value="${(i + 1).toString().padStart(2, "0")}" ${
+                    endMonth === (i + 1).toString().padStart(2, "0")
+                      ? "selected"
+                      : ""
+                  }>${m}</option>`
+              )
+              .join("")}
+          </select>
+          <label for="endMonth">ماه پایان را انتخاب کنید</label>
+        </div>
+      </div>
+    `;
+  }
 };
-
-// آپدیت فرم
-const updateInputForm = (year, month) => {
+const updateInputForm = (
+  year,
+  month,
+  startYear,
+  startMonth,
+  endYear,
+  endMonth
+) => {
   const inputFormContainer = document.createElement("div");
-  inputFormContainer.id = "inputFormContainer";
-  inputFormContainer.innerHTML = createInputForm(year, month);
+  const reportType = document.querySelector(
+    'input[name="reportType"]:checked'
+  ).value;
+  inputFormContainer.className = "input-form-container";
+  inputFormContainer.innerHTML = createInputForm(
+    reportType,
+    year,
+    month,
+    startYear,
+    startMonth,
+    endYear,
+    endMonth
+  );
 
   const updateButton = document.createElement("button");
   updateButton.id = "updateReportBtn";
   updateButton.textContent = "به‌روزرسانی گزارش";
   updateButton.addEventListener("click", function () {
-    const newYear = document.getElementById("reportYear").value;
-    const newMonth = document.getElementById("reportMonth").value;
-    generateReport(newYear, newMonth);
+    const reportType = document.querySelector(
+      'input[name="reportType"]:checked'
+    ).value;
+
+    if (reportType === "monthly") {
+      const newYear = document.getElementById("reportYear").value;
+      const newMonth = document.getElementById("reportMonth").value;
+      generateReport(newYear, newMonth);
+    } else {
+      const newStartYear = document.getElementById("startYear").value;
+      const newStartMonth = document.getElementById("startMonth").value;
+      const newEndYear = document.getElementById("endYear").value;
+      const newEndMonth = document.getElementById("endMonth").value;
+      generateReport(
+        null,
+        null,
+        newStartYear,
+        newStartMonth,
+        newEndYear,
+        newEndMonth
+      );
+    }
   });
 
-  const formContainer = document.createElement("div");
-  formContainer.id = "formContainer";
-  formContainer.appendChild(inputFormContainer);
-
-  const reportSection = document.getElementById("reportSection");
-  reportSection.appendChild(formContainer);
+  inputFormContainer.appendChild(updateButton);
+  document.getElementById("reportSection").appendChild(inputFormContainer);
 };
 const generateReport = async (
   year,
@@ -187,7 +296,6 @@ const generateReport = async (
       }
     }
 
-    // گرفتن اطلاعات از اکسل و نام فایل و نام شیت ها
     const fetchOptions = {
       headers: {
         "Cache-Control": "no-cache",
@@ -273,7 +381,6 @@ const generateReport = async (
       });
     });
 
-    // بررسی و تعریف متغیرهای زمان صرف شده
     let totalHours = 0;
     let remainingMinutes = 0;
     let timeSpentText = `${totalTimeSpent} دقیقه`;
@@ -285,66 +392,67 @@ const generateReport = async (
     }
 
     const taskReport = `
-  <div class="report-table">
-    <table border="1">
-      <thead>
-        <tr><th colspan="2" class="headTable">گزارش تعداد تسک‌ها بر اساس نوع</th></tr>
-        <tr><th>نوع تسک</th><th>تعداد استفاده شده</th></tr>
-      </thead>
-      <tbody>
-        ${taskSubjects
-          .map(
-            (subject, index) =>
-              `<tr><td>${subject}</td><td class="animated-number" data-end-value="${taskTypeCount[index]}">0</td></tr>`
-          )
-          .join("")}
-      </tbody>
-    </table>
-  </div>
-`;
+      <div class="report-table">
+        <table border="1">
+          <thead>
+            <tr><th colspan="2" class="headTable">گزارش تعداد تسک‌ها بر اساس نوع</th></tr>
+            <tr><th>نوع تسک</th><th>تعداد استفاده شده</th></tr>
+          </thead>
+          <tbody>
+            ${taskSubjects
+              .map(
+                (subject, index) =>
+                  `<tr><td>${subject}</td><td class="animated-number" data-end-value="${taskTypeCount[index]}">0</td></tr>`
+              )
+              .join("")}
+          </tbody>
+        </table>
+      </div>
+    `;
+
     const timeReport = `
-  <div class="report-table">
-    <table border="1">
-      <thead>
-        <tr><th colspan="3" class="headTable">گزارش زمانی و تعداد تسک‌ها بر اساس واحد</th></tr>
-        <tr><th>واحد</th><th>زمان صرف شده (دقیقه)</th><th>تعداد تسک</th></tr>
-      </thead>
-      <tbody>
-        ${Object.keys(unitTimeSpent)
-          .map(
-            (unit) => `
-              <tr>
-                <td>${unit}</td>
-                <td class="animated-number" data-end-value="${
-                  unitTimeSpent[unit]
-                }">0</td>
-                <td class="animated-number" data-end-value="${
-                  unitTaskCounts[unit] || 0
-                }">0</td>
-              </tr>
-            `
-          )
-          .join("")}
-      </tbody>
-    </table>
-  </div>
-`;
+      <div class="report-table">
+        <table border="1">
+          <thead>
+            <tr><th colspan="3" class="headTable">گزارش زمانی و تعداد تسک‌ها بر اساس واحد</th></tr>
+            <tr><th>واحد</th><th>زمان صرف شده (دقیقه)</th><th>تعداد تسک</th></tr>
+          </thead>
+          <tbody>
+            ${Object.keys(unitTimeSpent)
+              .map(
+                (unit) => `
+                  <tr>
+                    <td>${unit}</td>
+                    <td class="animated-number" data-end-value="${
+                      unitTimeSpent[unit]
+                    }">0</td>
+                    <td class="animated-number" data-end-value="${
+                      unitTaskCounts[unit] || 0
+                    }">0</td>
+                  </tr>
+                `
+              )
+              .join("")}
+          </tbody>
+        </table>
+      </div>
+    `;
 
     const totalTasksContainer = document.createElement("div");
     totalTasksContainer.className = "total-tasks-container";
     if (totalTimeSpent > 100) {
       totalTasksContainer.innerHTML = `
-    <p class="total-tasks">تعداد کل تسک‌ها: <span class="animated-number number-wrapper" data-end-value="${totalTasks}">0</span></p>
-    <p class="total-time-spent">مجموع زمان صرف شده: 
-      <span class="animated-number time-wrapper" data-end-value="${totalHours}">0</span> ساعت و 
-      <span class="animated-number time-wrapper" data-end-value="${remainingMinutes}">0</span> دقیقه
-    </p>
-  `;
+        <p class="total-tasks">تعداد کل تسک‌ها: <span class="animated-number number-wrapper" data-end-value="${totalTasks}">0</span></p>
+        <p class="total-time-spent">مجموع زمان صرف شده: 
+          <span class="animated-number time-wrapper" data-end-value="${totalHours}">0</span> ساعت و 
+          <span class="animated-number time-wrapper" data-end-value="${remainingMinutes}">0</span> دقیقه
+        </p>
+      `;
     } else {
       totalTasksContainer.innerHTML = `
-    <p class="total-tasks">تعداد کل تسک‌ها: <span class="animated-number number-wrapper" data-end-value="${totalTasks}">0</span></p>
-    <p class="total-time-spent">مجموع زمان صرف شده: <span class="animated-number time-wrapper" data-end-value="${totalTimeSpent}">0</span> دقیقه</p>
-  `;
+        <p class="total-tasks">تعداد کل تسک‌ها: <span class="animated-number number-wrapper" data-end-value="${totalTasks}">0</span></p>
+        <p class="total-time-spent">مجموع زمان صرف شده: <span class="animated-number time-wrapper" data-end-value="${totalTimeSpent}">0</span> دقیقه</p>
+      `;
     }
 
     const reportSummary = document.createElement("div");
@@ -353,45 +461,60 @@ const generateReport = async (
 
     const inputFormContainer = document.createElement("div");
     inputFormContainer.className = "input-form-container";
-    inputFormContainer.innerHTML = createInputForm(year, month);
+    inputFormContainer.innerHTML = createInputForm(
+      reportType,
+      year,
+      month,
+      startYear,
+      startMonth,
+      endYear,
+      endMonth
+    );
 
     const updateButton = document.createElement("button");
     updateButton.id = "updateReportBtn";
     updateButton.textContent = "به‌روزرسانی گزارش";
     updateButton.addEventListener("click", function () {
-      const newYear = document.getElementById("reportYear").value;
-      const newMonth = document.getElementById("reportMonth").value;
-      generateReport(newYear, newMonth);
+      if (reportType === "monthly") {
+        const newYear = document.getElementById("reportYear").value;
+        const newMonth = document.getElementById("reportMonth").value;
+        generateReport(newYear, newMonth);
+      } else {
+        const newStartYear = document.getElementById("startYear").value;
+        const newStartMonth = document.getElementById("startMonth").value;
+        const newEndYear = document.getElementById("endYear").value;
+        const newEndMonth = document.getElementById("endMonth").value;
+        generateReport(
+          null,
+          null,
+          newStartYear,
+          newStartMonth,
+          newEndYear,
+          newEndMonth
+        );
+      }
     });
     inputFormContainer.appendChild(updateButton);
-    inputFormContainer.appendChild(createPrintButton());
 
     reportSection.innerHTML = "";
     reportSection.appendChild(inputFormContainer);
     reportSection.appendChild(reportSummary);
-
     const reportTablesContainer = document.createElement("div");
     reportTablesContainer.className = "report-tables-container";
     reportTablesContainer.innerHTML = taskReport + timeReport;
     reportSection.appendChild(reportTablesContainer);
 
-    document
-      .getElementById("printReportBtn")
-      .addEventListener("click", printReport);
     hideLoading();
     reportContainer.style.display = "block";
     document.getElementById("formContainer").style.display = "none"; // پنهان کردن فرم اولیه
 
-    // اضافه کردن انیمیشن
     animateAllRandomNumbers();
-
   } catch (error) {
     console.error("Error:", error);
     showErrorPopup(`خطا در بارگذاری لطفا دوباره تلاش کنید`);
     hideLoading();
   }
 };
-// ساخت محتوی پرینت گزارش
 const generatePrintContent = (
   year,
   month,
